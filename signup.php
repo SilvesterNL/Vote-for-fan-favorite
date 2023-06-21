9df52bd8b8c4075b544e8b1c32f7e8a6251b5d05
Maddox <121097057+Maddox1122@users.noreply.github.com>
2023-06-21 11:38:33 +0200

    BG LOGIN

--- signup.php
+++ signup.php
@@ -22,7 +22,7 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Sign Up</title>
-  <link rel="stylesheet" href="loginsignup.css">
+  <link rel="stylesheet" href="/Challengeweek/vfff/assets/css/login.css">
   <link rel="shortcut icon" href="./assets/images/icons8-login-rounded-50.png" type="image/x-icon">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
